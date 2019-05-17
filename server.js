7682a2b741ce55fcf24563efeab35860acb8b42a
Leslie Thompson <lcrthompson@gmail.com>
2019-05-17 09:25:06 -0700

    error with post on action

--- server.js
+++ server.js
@@ -29,7 +29,41 @@ server.get("/actions", (req, res) => {
     .catch(err => {
       res
         .status(500)
-        .json({ message: "The action information could not be received", err });
+        .json({ message: "Error getting action information!", err });
+    });
+});
+
+//actions by id
+server.get("/actions/:id", (req, res) => {
+  const { id } = req.params;
+  actions
+    .get(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res
+          .status(404)
+          .json({ message: "There is no action with the specified ID!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error", err });
+    });
+});
+
+//post new action
+
+server.post("/actions", (req, res) => {
+  actions
+    .insert(req.body)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "There was an error creating a new action" });
     });
 });
 
