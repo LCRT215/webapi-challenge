f8d32ab3519d6bed1513678a6f0fc0957b24224a
Leslie Thompson <lcrthompson@gmail.com>
2019-05-17 09:16:03 -0700

    get all actions

--- server.js
+++ server.js
@@ -6,13 +6,33 @@ const morgan = require("morgan");
 
 const server = express();
 
-server.use(express.json());
+const actions = require("./data/helpers/actionModel");
+const projects = require("./data/helpers/projectModel");
 
+server.use(express.json());
 server.use(helmet());
 server.use(morgan("dev"));
 
 server.get("/", (req, res) => {
-  res.send("<h1>You are running!</h1>");
+  res.send("<h1>You are running now!</h1>");
 });
+
+//ACTIONS
+
+//get all of the actions
+server.get("/actions", (req, res) => {
+  actions
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "The action information could not be received", err });
+    });
+});
+
+//PROJECTS
 
 module.exports = server;
